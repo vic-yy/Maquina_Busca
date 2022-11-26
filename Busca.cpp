0f1ffdf8a89dc52f6975ac5f14c3c99bce5043b7
josedmass <josedmassucato@gmail.com>
2022-11-26 15:16:40 -0300

    Protótipo do Busca.cpp

---
+++ Busca.cpp
@@ -0,0 +1,24 @@
+#include "Busca.h"
+
+Busca::Busca() {}
+
+string Busca::RemoveCaractereEspecial(string s) {
+    
+}
+
+vector<string> Busca::normalizador(vector<string> x) {
+
+}
+
+
+void Busca::adicionar(string s) {
+   
+}
+
+void Busca::comparador (string nome, string texto, vector<string> termo_buscado) {
+    
+}
+
+void Busca::impressao () {
+    
+}
