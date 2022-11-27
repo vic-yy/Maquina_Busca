6cafab697b0220bf2aa1d145b13358ae6eeff5e3
josedmass <josedmassucato@gmail.com>
2022-11-26 22:24:49 -0300

    Explicitando Busca.h

--- Busca.h
+++ Busca.h
@@ -14,6 +14,7 @@ class Busca {
 
 public:
 
+    // Construtor, que cria um IndexInvertido vazio
     Busca();
 
     // Acessa o set de IndexInvertido
@@ -22,14 +23,21 @@ public:
     // Recebe uma cópia de IndexInvertido
     map<string, set<string>> Index();
 
+    // Apaga os caracteres especiais (acentos, hífens, etc)
     string RemoveCaractereEspecial(string s);
 
+    // Formata todas as palavras de um vector<string>
     vector<string> normalizador(vector<string> x);
 
-    void adicionar(string s); // adiciona itens ao indice de IndexInvertido
+    // Adiciona itens ao indice de IndexInvertido
+    void adicionar(string s);
 
+    // Compara um "texto" advindo do arquivo com a consulta do usuário em "termo_buscado".
+    // Caso todas as palavras da consulta estejam presentes no texto, fazemos a indexação, 
+    // colocando o "nome" no set<string> de IndexInvertido.
     void comparador (string nome, string texto, vector<string> termo_buscado);
 
+    // Imprimimos os elementos do set<string> de IndexInvertido.
     void impressao (); 
 
 private:
